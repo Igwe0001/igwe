f0631e021c0aa45a92b63cef28cd4171012bbeb3
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-03-25 15:23:24 +0100

    structure addition

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const hamburger = document.querySelector('.hamburger')
-const nav = document.querySelector('nav')
-const line = document.querySelectorAll('.line')
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const line = document.querySelectorAll(".line");
+const link = document.querySelectorAll(".link");
 
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("nav-toggle");
+  line.forEach((e) => {
+    e.classList.toggle("trans");
+  });
+});
 
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('nav-toggle')
-    line.forEach(e => {
-        e.classList.toggle('trans')
-    })
-})
+link.forEach((e) => {
+  e.addEventListener("click", () => {
+    nav.classList.remove("nav-toggle");
+    line.forEach((e) => {
+      e.classList.toggle("trans");
+    });
+  });
+});
