a6ebbac703571d2f3b8a76faec7be30cee2a5c55
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-04-04 16:33:27 +0100

    added animations

--- script.js
+++ script.js
@@ -2,6 +2,44 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
 const line = document.querySelectorAll(".line");
 const link = document.querySelectorAll(".link");
+const intro = document.querySelector('.intro');
+const logo = document.querySelector('.logo-header');
+const logospan = document.querySelectorAll('.intro-logo');
+
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+
+    logospan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("acting");
+      }, (idx + 1) * 400)  
+    })
+
+    setTimeout(() => {
+      logospan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove('acting');
+          span.classList.add('fade')
+        }, (idx + 1) * 50)
+      })
+    }, 2000)
+    
+    
+    setTimeout(() => {
+    intro.style.top ='-100vh'
+  }, 2300)
+
+
+
+
+  })
+})
+
+
+
+
+
+
 
 window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
@@ -20,6 +58,42 @@ link.forEach((e) => {
     nav.classList.remove("nav-toggle");
     line.forEach((e) => {
       e.classList.toggle("trans");
+
     });
   });
 });
+
+
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  let reveals = document.querySelectorAll('.reveal');
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('show');
+    }
+    else {
+      reveals[i].classList.remove('show');
+    }
+  }
+}
+
+let loader = document.getElementById('preloader');
+window.addEventListener('load', function () {
+  loader.style.display = 'none';
+})
